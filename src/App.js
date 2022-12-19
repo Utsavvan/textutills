9b4d9d7ba0108f23240fcf99056327454f5e0d04
Utsav <Utsav1pariya@gmail.com>
2022-12-19 17:23:20 +0530

    added dark mode to about page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './components/navbar.mjs';
 import TextForm from './components/TextForm.mjs';
+import About from './components/About.js';
 
 function App() {
   return (
     <>
     {/* we can pass values that is going to render in rect components in key = value pairs  */}
       <Navbar title='hubsl' menu1="HOME"/>
-      <TextForm heading="Example of props"/>
+      {/* <TextForm heading="Example of props"/> */}
+      <About/>
     </>
   );
 }
--- src/components/TextForm.mjs
+++ src/components/TextForm.mjs
@@ -9,19 +9,32 @@ function TextForm(props) {
     const handleClickLw = () => {
         setText(text.toLowerCase())
     }
+    const handleClickCl = () => {
+        setText("")
+    }
     const onChanged = (event) => {
         setText(event.target.value);
     }
+
+	const handleClickCopyText = () => {
+		var text = document.getElementById("textbox")
+		text.select()
+		// text.setSelectionRange(0,9999)
+        navigator.clipboard.writeText(text.value)
+    }
+
     return (
         <React.Fragment>
             <div className="container my-3">
                 <h1>{props.heading}</h1>
                 <div className="form-group">
                     <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
-                    <textarea className="form-control" value={text} onChange={onChanged} id="exampleFormControlTextarea1" rows="8"></textarea>
+                    <textarea className="form-control" value={text} onChange={onChanged} id="textbox" rows="8"></textarea>
                 </div>
                 <button onClick={handleClickUp} className="btn btn-primary mt-3 mx-3">Uppercase</button>
                 <button onClick={handleClickLw} className="btn btn-primary mt-3 mx-3">Lowercase</button>
+                <button onClick={handleClickCl} className="btn btn-primary mt-3 mx-3">Clear</button>
+                <button onClick={handleClickCopyText} className="btn btn-primary mt-3 mx-3">Copy Text</button>
             </div>
             <div className="container">
                 <h1>This Text has {text.split(' ').length} Words And {text.length} Characters</h1>
