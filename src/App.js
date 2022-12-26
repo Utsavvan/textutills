2a7ad632b3c983f8b61922e3d5fa4f912ac17803
Utsav <Utsav1pariya@gmail.com>
2022-12-26 21:03:37 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
 import Navbar from './components/navbar.mjs';
 import TextForm from './components/TextForm.mjs';
-// import About from './components/About.js';
+import About from './components/About.js';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light')
@@ -11,12 +18,12 @@ function App() {
 
   const showAlert = (type, message) => {
     setAlert({
-      type : type,
-      msg : message,
+      type: type,
+      msg: message,
     })
-    setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null)
-    },1000)
+    }, 1000)
   }
 
   const switchClick = (data) => {
@@ -24,24 +31,36 @@ function App() {
     if (mode === 'light') {
       setMode('dark')
       document.body.style.backgroundColor = "#001e3c"
-      showAlert('success',"Dark mode is enabled")
+      showAlert('success', "Dark mode is enabled")
       document.title = 'Textutils - Dark Mode'
     } else {
       setMode('light')
       document.body.style.backgroundColor = "white"
-      showAlert('success',"Light mode is enabled")
+      showAlert('success', "Light mode is enabled")
       document.title = 'Textutils - Light Mode'
     }
   }
 
   return (
-    <>
-      {/* we can pass values that is going to render in rect components in key = value pairs  */}
-      <Navbar title='hubsl' mode={mode} switchClick={switchClick} menu1="HOME" />
-      <Alert alert={alert} showalert={showAlert} />
-      <TextForm mode={mode} showalert={showAlert} heading="This is title by props" />
-      {/* <About/> */}
-    </>
+    <React.Fragment>
+      <Router>
+        {/* we can pass values that is going to render in rect components in key = value pairs  */}
+        <Navbar title='hubsl' mode={mode} switchClick={switchClick} menu1="HOME" />
+        <Alert alert={alert} showalert={showAlert} />
+
+        <Routes >
+
+          <Route path="/" exact element={
+            <TextForm mode={mode} showalert={showAlert} heading="This is title by props" />
+          } />
+
+          <Route path="/about" exact element={
+            <About />
+          } />
+
+        </Routes >
+      </Router>
+    </React.Fragment>
   );
 }
 
--- src/components/navbar.mjs
+++ src/components/navbar.mjs
@@ -1,5 +1,6 @@
 import React from 'react'
 import propTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
 
 // if we passed props in react component than we use "props" or any other value as a parameter to our function 
 function navbar(props) {
@@ -9,17 +10,17 @@ function navbar(props) {
             <div className="container-fluid">
                 {/* we can access values of props by {functionParameterValue.propKey} ,like here we passed function parameter as "props" and 
                 we passed prop key in components as "title" so we are using {props.title}*/}
-                <a className="navbar-brand" href="/">{props.title}</a>
+                <NavLink className="navbar-brand" to="/">{props.title}</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">{props.menu1}</a>
+                            <NavLink className="nav-link active" aria-current="page" to="/">{props.menu1}</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">Link</a>
+                            <NavLink className="nav-link" to="/about">About</NavLink>
                         </li>
                     </ul>
                     {/* <form className="d-flex">
